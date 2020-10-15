b995a5204372115c7290370341ede3566d4bf58d
Tyler Amos <tamos5@jhu.edu>
2020-10-15 16:05:51 -0400

    main now reads in files

--- csim.cpp
+++ csim.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-using std::cin;
-using std::cout;
-using std::endl;
-
+#include <string>
+#include <sstream>
+#include <fstream>
+using std::cin; using std::cout; using std::endl;
+using std::string;
+using std::getline;
 int main(){
-        cout << "Hello World!" << endl;
-
+	char ls = '\0'; // store instruction
+	string address; // store address
+	string line; // get whole lines from file
+	
+	// start main loop, reading 1 instruction at a time
+	while (getline(cin,line)) {
+		ls = line[0];
+		address = line.substr(2,10);
+		cout << ls << " " << address << endl;
+	}
+	return 0;
 }
